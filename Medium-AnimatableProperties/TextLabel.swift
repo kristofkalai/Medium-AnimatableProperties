8225a22d5f8ba429fac19dfe08e72c0d48340dbb
Kristóf Kálai <kristof.kalai@supercharge.io>
2024-01-01 01:48:35 +0100

    feat: add CATextLayer support

---
+++ Medium-AnimatableProperties/TextLabel.swift
@@ -0,0 +1,140 @@
+//
+//  TextLabel.swift
+//  Medium-AnimatableProperties
+//
+//  Created by Kristóf Kálai on 01/01/2024.
+//
+
+import UIKit
+
+final class TextLabel: UIView {
+    enum Text {
+        case string(String)
+        case attributedString(NSAttributedString)
+        case none
+    }
+
+    var text: Text {
+        get {
+            if let attributedString = layer.string as? NSAttributedString {
+                .attributedString(attributedString)
+            } else if let string = layer.string as? String {
+                .string(string)
+            } else {
+                .none
+            }
+        }
+        set {
+            switch newValue {
+            case let .string(string): layer.string = string
+            case let .attributedString(attributedString): layer.string = attributedString
+            case .none: layer.string = nil
+            }
+        }
+    }
+
+    var font: UIFont? {
+        get {
+            func cast(_ cfString: CFString?) -> String? {
+                if let cfString {
+                    cfString as NSString as String
+                } else {
+                    nil
+                }
+            }
+
+            let name: String? = {
+                switch layer.font {
+                case let ctFont as CTFont: cast(CTFontCopyName(ctFont, kCTFontPostScriptNameKey))
+                case let cgFont as CGFont: cast(cgFont.postScriptName)
+                case let string as NSString: string as String
+                case let string as String: string
+                default: nil
+                }
+            }()
+
+            return name.flatMap { UIFont(name: $0, size: layer.fontSize) }
+        }
+        set {
+            layer.font = newValue
+        }
+    }
+
+    var fontSize: CGFloat {
+        get {
+            layer.fontSize
+        }
+        set {
+            layer.fontSize = newValue
+        }
+    }
+
+    var foregroundColor: UIColor? {
+        get {
+            layer.foregroundColor.map { .init(cgColor: $0) }
+        }
+        set {
+            layer.foregroundColor = newValue?.cgColor
+        }
+    }
+
+    var isWrapped: Bool {
+        get {
+            layer.isWrapped
+        }
+        set {
+            layer.isWrapped = newValue
+        }
+    }
+
+    var truncationMode: CATextLayerTruncationMode {
+        get {
+            layer.truncationMode
+        }
+        set {
+            layer.truncationMode = newValue
+        }
+    }
+
+    var alignmentMode: CATextLayerAlignmentMode {
+        get {
+            layer.alignmentMode
+        }
+        set {
+            layer.alignmentMode = newValue
+        }
+    }
+
+    var allowsFontSubpixelQuantization: Bool {
+        get {
+            layer.allowsFontSubpixelQuantization
+        }
+        set {
+            layer.allowsFontSubpixelQuantization = newValue
+        }
+    }
+
+    override class var layerClass: AnyClass {
+        CATextLayer.self
+    }
+
+    override var layer: CATextLayer {
+        super.layer as! CATextLayer
+    }
+
+    override init(frame: CGRect = .zero) {
+        super.init(frame: frame)
+        commonInit()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+}
+
+extension TextLabel {
+    private func commonInit() {
+        layer.contentsScale = UIScreen.main.scale
+    }
+}
--- Medium-AnimatableProperties/ViewController.swift
+++ Medium-AnimatableProperties/ViewController.swift
@@ -8,8 +8,8 @@
 import UIKit
 
 final class ViewController: UIViewController {
-    private let subview: UILabel = {
-        let view = UILabel()
+    private let subview: TextLabel = {
+        let view = TextLabel()
         view.translatesAutoresizingMaskIntoConstraints = false
         view.backgroundColor = .blue
         view.heightAnchor.constraint(equalToConstant: 100).isActive = true
@@ -24,8 +24,8 @@ final class ViewController: UIViewController {
         subview.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         subview.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
 
-        subview.text = "TEST"
-        subview.textColor = .red
+        subview.text = .string("TEST")
+        subview.foregroundColor = .red
 
         DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
             self.animate()
@@ -34,7 +34,7 @@ final class ViewController: UIViewController {
 
     private func animate() {
         UIView.animate(withDuration: 3) {
-            self.subview.textColor = .green
+            self.subview.foregroundColor = .green
         }
     }
 }
